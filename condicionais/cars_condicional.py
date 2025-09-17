f020b9b218c6078aa6ecc8aeb294376507efbc5a
Glauco Vitti Bonturi <69531088+gvittibonturi@users.noreply.github.com>
2025-09-17 11:34:31 -0300

    Add files via upload

---
+++ condicionais/cars_condicional.py
@@ -0,0 +1,22 @@
+car = 'bmw'
+print("Is car == 'bmw'? I predict True.")
+print(car == 'bmw')
+
+fruta = 'pera'
+print("Is Pera == 'Pera'? I predict True.")
+print(fruta.title() == 'Pera')
+
+print("\nIs car == 'mercedes'? I predict False")
+print(car == 'mercedes')
+
+fruta = 'maçã'
+print("Is maçã == 'melancia'? I predict True.")
+print(fruta == 'melancia')
+
+lar = 'apartamento'
+print("Is lar == 'apartamento'? I preedict True")
+print(lar == 'apartamento')
+
+print("Is lar == 'casa'? I predict False")
+print(lar == 'casa')
+
