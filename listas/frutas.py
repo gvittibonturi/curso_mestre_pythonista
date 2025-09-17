c99f21321d190012fed2a9491131b526b492dfda
Glauco Vitti Bonturi <69531088+gvittibonturi@users.noreply.github.com>
2025-09-17 11:11:52 -0300

    Add files via upload

---
+++ listas/frutas.py
@@ -0,0 +1,15 @@
+favorite_fruits = ['laranja', 'morango', 'pitaya']
+
+if 'laranja' in favorite_fruits:
+	print("Você realmnete gosta de laranja!")
+if 'abacate' in favorite_fruits:
+	print("Você realmente gosta de abacate!")
+if 'morango' in favorite_fruits:
+	print("Você realmente gosta de morango!")
+if 'pêssego' in favorite_fruits:
+	print("Você realmente gosta de pêssego!")
+if 'pitaya' in favorite_fruits:
+	print("Você realmente gosta de pitaya!")
+
+
+ 
