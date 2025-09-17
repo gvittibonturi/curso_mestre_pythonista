acdbfbc29fc4b4bfafb77da4f4ce6702db0c635e
Glauco Vitti Bonturi <69531088+gvittibonturi@users.noreply.github.com>
2025-09-17 11:22:36 -0300

    Add files via upload

---
+++ dicionarios/pessoas.py
@@ -0,0 +1,11 @@
+pessoa_0 = {
+    'first_name': 'Glauco',
+     'last_name': 'Bonturi',
+     'age': '46',
+     'city': 'Goiânia',
+     }
+     
+print(pessoa_0['first_name'])
+print(pessoa_0['last_name'])
+print(pessoa_0['age'])
+print(pessoa_0['city'])
