fc5ce2d64e55205a050ad6548ce09f7b2ae654ea
Glauco Vitti Bonturi <69531088+gvittibonturi@users.noreply.github.com>
2025-09-17 11:36:07 -0300

    Add files via upload

---
+++ condicionais/toppings.py
@@ -0,0 +1,22 @@
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+prato = 'pineapple'
+prato2 = 'macarrão'
+
+if prato in requested_toppings:
+	print(prato.title() + ", está na lista dos mehlores pratos")
+else:
+	print(prato.title() + ", não está na lista dos melhores pratos")
+	
+if prato2 not in requested_toppings:
+	print(prato2.title() + ", não está na lista dos melhores pratos")
+else:
+	print(prato2.title() + ", está na lista dos melhores pratos")
+
+
+
+
+#if requested_toppoing != 'anchovies':
+#	print("Hold the anchovies:")
+
+	
+
