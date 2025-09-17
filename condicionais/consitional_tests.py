055704fa77705a33dc09d8a6c977c250c0102f7c
Glauco Vitti Bonturi <69531088+gvittibonturi@users.noreply.github.com>
2025-09-17 11:35:10 -0300

    Add files via upload

---
+++ condicionais/consitional_tests.py
@@ -0,0 +1,42 @@
+motos = ['bmw', 'yamaha', 'honda']
+
+if motos[0] == 'bmw':
+	print("Essa marca de moto, " + motos[0].title() + " é muito top")
+	
+if motos[1] != 'honda':
+	print("Essa marca de moto, " + motos[1].title() + " não é Honda")
+
+motos[2] = 'Honda'
+
+if motos[2].lower() == 'honda':
+	print("Essa moto " + motos[2] + " é a melhor")
+	
+idade = 46
+idade2 = 84
+
+if idade != 47:
+	print("Essa não é minha idade")
+	
+if idade == 46:
+	print("Essa é minha idade verdadeira")
+	
+if idade >= 40:
+	print("Tudo ceto")
+	
+if idade >= 40:
+	print("Tudo ceto")
+	
+if idade > 46 or idade2 < 86:
+	print("Idade dentro do padrão")
+else:
+	print("Idade fora do padrão")
+
+if idade != 49 and idade2 <= 83:
+	print("Idade dentro do padrão")
+else:
+	print("Idade fora do padrão!")
+
+	
+
+	
+	
