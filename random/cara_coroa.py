743f8b446a3cdbe7e8b45ebab926a7075c574e84
Glauco Vitti Bonturi <69531088+gvittibonturi@users.noreply.github.com>
2025-09-18 09:35:09 -0300

    Add files via upload

---
+++ random/cara_coroa.py
@@ -0,0 +1,13 @@
+import random
+
+# Desafio 1
+moeda = ['cara', 'coroa']
+
+print(random.choice(moeda))
+
+# Desafio 2
+nomes = ['Glauco', 'Antônio', 'Ennio', 'Miguel', 'Gustavo', 'Eduardo']
+print(random.choice(nomes))
+
+# Desafio 3
+print(random.randint(10, 100))
